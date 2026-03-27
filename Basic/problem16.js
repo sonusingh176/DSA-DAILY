fd07f1db6ca2a4ede1a193535800986c04067846
sonusingh176 <sonusingh82601@gmail.com>
2026-03-27 23:52:03 +0530

    fizz Buzz

--- Basic/problem16.js
+++ Basic/problem16.js
@@ -2,65 +2,40 @@
 //Armstrong number of 3 digits, the sum of cubes of each digit is equal to the number itself. 
 // For example, 153 is an Armstrong number because > 153 = 1*1*1 + 5*5*5 + 3*3*3 
 
-function  CheckArmstrongNumber(num){
+function checkArmstrong(num){
 
-    const casttostr= num.toString();
+    let str = num.toString();
+    let len = str.length;
 
-    if (casttostr.length > 3) {
-        throw new Error("This method only works for 3-digit numbers");
+    let result=0; //153
+
+    for(let i=0; i < len ;i++){
+         let digit=Number(str[i]);
+       // result += Math.pow(digit, len) ;
+      result += digit ** len;   // (1)^3 + (5)^3 + (3)^3
     }
-
-    let result=0;
-    for(let i=0;i<casttostr.length;i++){
-
-        result+= parseInt(casttostr[i]*casttostr[i]*casttostr[i]) //
-
-    }
-
-    if(num==result){
-        return true;
-    }
-
-    //return result;
-
+    return num === result
+     
 }
 
-// console.log(CheckArmstrongNumber(153));
-// console.log(CheckArmstrongNumber(1634));
-// console.log(CheckArmstrongNumber(370));
+//console.log("is Armstrong :",checkArmstrong(9474)); 
 
-// =================================================================================================
+// ===============================================================
 
-// 153 % 10 = 3
-// 15   % 10 = 5
-// 1   % 10 = 1
+function checkArmstrong2(num){
 
-//Above Solution is not good . Don't even write this type of solution in interviews.
+    let  original = num;
+    let sum =0;
+    let totaldigit = num.toString().length;
 
+   while (num > 0) {  // 153 > 0   | 15 > 0  | 1 > 0  | 0> 0 (x)
+    let digit= num % 10; // 153 % 10 = 3   |  15 % 3 = 5   | 1 % 10 = 1 
+    sum+=Math.pow(digit,totaldigit);  // 3 = 3*3*3  sum = 27   |  5 =5*5*5 = sum = 27 + 125 = 152   |  1= 1*1*1 , sum = 152 + 1 => 153
+    num=Math.floor(num / 10);  // 153 / 10 = 15.3  => Math.floor(15.3) , 15   | 15 / 10 =1.5   => Math.floor(1.5)  => 1 |  1 / 10 => 0.5   Math.floor(0.5) => 0
+   }
 
-function CheckArmstrongNumber2(num)
-{
-    let original=num;
-    let sum=0;
-
-    // step 1: count digit
-    let totaldigits=num.toString().length;
-
-    // step 2: loop to process each digit
-    while (num>0) {
-        let digit =num % 10 ;  // last digit
-        sum += Math.pow(digit,totaldigits);  // power add
-        num = Math.floor(num/10); // remove last digit
-    }  
-
-     // step 3: compare
-     if (sum === original) {
-        return "Armstrong Number";
-    } else {
-        return "Not Armstrong Number";
-    }
-
+  return sum === original;
+   
 }
 
-console.log(CheckArmstrongNumber2(153)); // Armstrong Number
-console.log(CheckArmstrongNumber2(123)); 
+console.log("is artmstrong", checkArmstrong2(9474)); 
